edc7f86d69e468b40dc17d1dda60768c867f5a77
Mokhlesur Mahin <mokhlesurr031@gmail.com>
2024-11-03 02:40:43 +0600

    update: response model

--- model/error.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type ErrorResponse struct {
-	Code    int    `json:"code"`    // Error code
-	Message string `json:"message"` // Detailed error message
-}
--- model/response.go
+++ model/response.go
@@ -1,5 +1,10 @@
 package model
 
+type ErrorResponse struct {
+	Code    int    `json:"code"`    // Error code
+	Message string `json:"message"` // Detailed error message
+}
+
 type SuccessResponse struct {
 	Code    int         `json:"code"`    // Status code
 	Message string      `json:"message"` // Success message
